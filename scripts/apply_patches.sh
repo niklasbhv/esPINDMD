0cf0c3b70ecd96f91110788297841044ee182deb
Niklas <nik_mey@uni-bremen.de>
2025-07-14 21:18:12 +0000

    Fixed apply_patches scripts

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -4,4 +4,4 @@ echo "Updating submodules..."
 git submodule update --init --recursive
 
 echo "Applying patches..."
-git -C ext_lib/SdFat apply ../../patches/sdfat.patch
+git -C lib/SdFat apply ../../patches/sdfat.patch
